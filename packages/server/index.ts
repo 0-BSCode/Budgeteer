7999791fcd53f34d9b9e2eba63be552b2ba0e1d1
0-BSCode <bryanipsanchez@gmail.com>
2024-11-04 23:40:52 +0800

    Initialize hono

--- packages/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { type Transaction } from "@budgeteer/types";
-
-const transaction: Transaction = {
-    id: "1",
-    title: "Food",
-    amount: 20,
-    date: new Date(),
-}
-console.log(transaction)
