89a46eae5da31542f6466b1e7a48191368db8976
0-BSCode <bryanipsanchez@gmail.com>
2024-11-04 23:35:44 +0800

    Add types to server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { type Transaction } from "@budgeteer/types";
+
+const transaction: Transaction = {
+    id: "1",
+    title: "Food",
+    amount: 20,
+    date: new Date(),
+}
+console.log(transaction)
