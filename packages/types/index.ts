05357e2d591e7f78e997ffff466352b7ea67f701
0-BSCode <bryanipsanchez@gmail.com>
2024-11-08 00:53:46 +0800

    feat(misc): initialize repository for transaction CRUD

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -4,6 +4,9 @@ export * from './src/entities/transactions/transaction.dto';
 export * from './src/entities/transactions/transaction-create.dto';
 export * from './src/entities/transactions/transaction-update.dto';
 
+// Abstracts
+export * from './src/abstracts/repositories/transaction-repository.abstract';
+
 // Enums
 export * from './src/enums/node-env.enum';
 export * from './src/enums/transaction-type.enum';
---
+++ packages/types/src/abstracts/repositories/transaction-repository.abstract.ts
@@ -0,0 +1,13 @@
+import type { TransactionCreateDto } from "~/entities/transactions/transaction-create.dto"
+import type { TransactionUpdateDto } from "~/entities/transactions/transaction-update.dto"
+import type { TransactionDto } from "~/entities/transactions/transaction.dto"
+
+export type ITransactionRepository = {
+  // TODO: Add remaining methods with users
+  // findByUser: (userId: number) => Promise<TransactionDto[]>
+  findById: (id: number) => Promise<TransactionDto | null>
+  create: (dto: TransactionCreateDto) => Promise<TransactionDto>
+  update: (id: number, dto: TransactionUpdateDto) => Promise<TransactionDto>
+  delete: (id: number) => Promise<void>
+  convertToDto: (data: unknown) => TransactionDto
+}
