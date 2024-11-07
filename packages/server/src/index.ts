9d7a5aea1acee3f3d2a12c9ff327d595f9c87f5e
0-BSCode <bryanipsanchez@gmail.com>
2024-11-08 01:05:54 +0800

    feat(misc): initialize routes for transactions

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono"
+import authorApp from "./routes/transaction.route"
 
 const app = new Hono()
-app.get("/", c => c.text("Hello Bun!"))
+app.get("/", c => c.text("OK!"))
+
+app.route("/api/transaction", authorApp)
 
 export default app
