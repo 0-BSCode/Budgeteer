991d7f14ad1bc9ee34e5179107104abfb47b4a30
Luzefiru <ian.deejay.iv@gmail.com>
2024-11-08 12:53:37 +0800

    feat(auth): integrate shadcn & finish auth pages
    resolves TR-3

---
+++ packages/client/src/app/(app)/auth/sign-up/page.tsx
@@ -0,0 +1,60 @@
+import { Metadata } from "next"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import { SignUpForm } from "@/features/auth/sign-up-form"
+
+export const metadata: Metadata = {
+  title: "Sign Up | Budgeteer",
+}
+
+export default function SignUpPage() {
+  return (
+    <main className="h-full w-full">
+      <div className="container relative h-full flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Button asChild variant="ghost" className="absolute right-4 top-4 md:right-8 md:top-8">
+          <Link href="/auth/login">Log In</Link>
+        </Button>
+
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Budgeteer
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;Budgeteer allows me to budget my expenses properly while tracking my income. Reaching my
+                financial goals has never been easier!&rdquo;
+              </p>
+              <footer className="text-sm">Anon</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Create a Budgeteer account</h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your desired username & password below to create your account
+              </p>
+            </div>
+            <SignUpForm />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ packages/client/src/features/auth/sign-up-form.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { useState } from "react"
+import { LoaderCircle } from "lucide-react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+interface SignUpFormProps {
+  className?: string
+}
+
+export function SignUpForm({ className }: SignUpFormProps) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault()
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-3">
+          <div className="grid gap-1">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              placeholder="Enter your username"
+              autoCapitalize="none"
+              autoComplete="username"
+              autoCorrect="off"
+              disabled={isLoading}
+              required
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              type="password"
+              disabled={isLoading}
+              required
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+            Log In
+          </Button>
+        </div>
+      </form>
+      <div className="text-center text-sm text-muted-foreground">
+        Already have an account?{" "}
+        <Link href="/auth/login" className="underline underline-offset-4 hover:text-primary">
+          Log In
+        </Link>{" "}
+        instead.
+      </div>
+    </div>
+  )
+}
