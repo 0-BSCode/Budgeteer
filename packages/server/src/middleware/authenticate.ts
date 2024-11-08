abd42247f4a24b2ee52849a2228777fbf8f84e75
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-11-08 22:30:16 +0800

    feat(auth): create authenticate middleware
    re TR-2

---
+++ packages/server/src/middleware/authenticate.ts
@@ -0,0 +1,31 @@
+import { verify } from "hono/jwt"
+import { createMiddleware } from "hono/factory"
+import { envConfig } from "~/services/config-service"
+import type { SignatureKey } from "hono/utils/jwt/jws"
+
+const authenticate = createMiddleware(async (c, next) => {
+  const authHeader = c.req.header("authorization")
+
+  if (!authHeader) {
+    return c.json({ msg: "No authorization header found" }, 403)
+  }
+
+  const token = authHeader.split(" ")[1]
+
+  try {
+    const decodedPayload = await verify(token, envConfig.JWT_SECRET as SignatureKey)
+
+    const { username, id } = decodedPayload
+
+    // Set variables for user id and username in request context
+    c.set("id", id)
+    c.set("username", username)
+  } catch (err) {
+    console.log(err)
+    return c.json({ msg: `An error occurred while verifying token` }, 403)
+  }
+
+  await next()
+})
+
+export { authenticate }
--- packages/server/src/router/users/users.ts
+++ packages/server/src/router/users/users.ts
@@ -2,18 +2,22 @@ import { Hono } from "hono"
 import { db } from "~/infrastructure/typeorm-data-service"
 import { usersTable } from "~/infrastructure/typeorm-data-service/models/user.model"
 import { eq } from "drizzle-orm"
-import { zValidator } from "@hono/zod-validator"
-import { z } from "zod"
-const user = new Hono().basePath("/user")
 
-const schema = z.object({
-  id: z.string().pipe(z.coerce.number()),
-})
+import { authenticate } from "~/middleware/authenticate"
 
-user.get("/:id", zValidator("param", schema), async c => {
-  const { id } = c.req.valid("param")
+type Variables = {
+  id: string
+}
 
-  const foundUser = await db.select().from(usersTable).where(eq(usersTable.id, id))
+const user = new Hono<{ Variables: Variables }>().basePath("/user")
+
+user.get("/", authenticate, async c => {
+  const id = c.get("id")
+
+  const foundUser = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.id, parseInt(id)))
 
   if (!foundUser) {
     return c.json({ msg: "User not found!" }, 400)
