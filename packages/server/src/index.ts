ca962616b1becd60fda7f19173e947cb591a90c1
0-BSCode <bryanipsanchez@gmail.com>
2024-11-09 19:13:39 +0800

    feat(server index): implemented global http-exception handler
    re TR-5

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,23 @@
 import { Hono } from "hono"
 import transactionApi from "./router/transactions/transactions.router"
+import { HTTPException } from "hono/http-exception"
+import { HttpStatusEnum, type ResponseDto } from "@budgeteer/types"
 
 const app = new Hono()
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse()
+  }
+
+  const response: ResponseDto<null> = {
+    status: HttpStatusEnum.INTERNAL_SERVER_ERROR,
+    data: null,
+    message: err.message,
+  }
+  return c.json(response)
+})
+
 app.get("/", c => c.text("OK!"))
 
 app.route("/api/transactions", transactionApi)
