ae2c040dadce91a6af507e9312d9672f8f119379
0-BSCode <bryanipsanchez@gmail.com>
2024-11-10 20:53:09 +0800

    feat(transaction model): added column for transaction category
    re TR-5

--- packages/server/src/infrastructure/typeorm-data-service/models/transaction.model.ts
+++ packages/server/src/infrastructure/typeorm-data-service/models/transaction.model.ts
@@ -1,14 +1,24 @@
 import { integer, pgTable, varchar, date, pgEnum, real } from "drizzle-orm/pg-core"
-import { enumToPgEnum } from "../utils/enumToPgEnum"
-import { MAX_TRANSACTION_DESCRIPTION_LENGTH, TransactionTypeEnum } from "@budgeteer/types"
+import { enumsToPgEnum, enumToPgEnum } from "../utils/enumToPgEnum"
+import {
+  ExpenseCategoryEnum,
+  IncomeCategoryEnum,
+  MAX_TRANSACTION_DESCRIPTION_LENGTH,
+  TransactionTypeEnum,
+} from "@budgeteer/types"
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm"
 
 export const transactionTypeEnum = pgEnum("transaction_type", enumToPgEnum(TransactionTypeEnum))
+export const transactionCategoryEnum = pgEnum(
+  "transaction_category",
+  enumsToPgEnum([IncomeCategoryEnum, ExpenseCategoryEnum]),
+)
 
 export const transactionsTable = pgTable("transactions", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   description: varchar({ length: MAX_TRANSACTION_DESCRIPTION_LENGTH }).notNull(),
   type: transactionTypeEnum().notNull().default(TransactionTypeEnum.EXPENSE),
+  category: transactionCategoryEnum().notNull(),
   amount: real().notNull().default(0),
   createdAt: date().defaultNow().notNull(),
   updatedAt: date().defaultNow().notNull(),
