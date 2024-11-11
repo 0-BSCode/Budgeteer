32c56bbfea78d20210c22b65f293cf1e60ab45b5
0-BSCode <bryanipsanchez@gmail.com>
2024-11-11 09:09:01 +0800

    refactor(types): remove duplicate exports

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -17,17 +17,10 @@ export * from "./src/abstracts/use-cases/users-use-cases.abstract"
 export * from "./src/abstracts/use-cases/auth-use-cases.abstract"
 export * from "./src/abstracts/use-cases/transaction-use-cases.abstract"
 
-// Abstracts
-export * from "./src/abstracts/repositories/transaction-repository.abstract"
-export * from "./src/abstracts/services/data-service.abstract"
-
 // Enums
-export * from "./src/enums/node-env.enum"
-export * from "./src/enums/transaction-type.enum"
 export * from "./src/enums/http-status.enum"
 export * from './src/enums/node-env.enum';
 export * from './src/enums/transaction-type.enum';
-export * from './src/enums/http-status.enum';
 export * from './src/enums/transaction-category.enum';
 
 // Constants
