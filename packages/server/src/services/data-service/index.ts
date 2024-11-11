d7dab1ba7613c1532fc9302e4037b9cfb4c641b4
0-BSCode <bryanipsanchez@gmail.com>
2024-11-11 08:49:51 +0800

    chore(drizzle data service): rename data service to drizzle instead of typeorm

--- packages/server/src/services/data-service/index.ts
+++ packages/server/src/services/data-service/index.ts
@@ -1,7 +1,7 @@
 import type { IDataService } from "@budgeteer/types"
 
-import { userRepository } from "~/infrastructure/typeorm-data-service/repositories/user.repository"
-import { transactionRepository } from "~/infrastructure/typeorm-data-service/repositories/transaction.repository"
+import { userRepository } from "~/infrastructure/drizzle-data-service/repositories/user.repository"
+import { transactionRepository } from "~/infrastructure/drizzle-data-service/repositories/transaction.repository"
 
 export const DataService: IDataService = {
   users: userRepository,
