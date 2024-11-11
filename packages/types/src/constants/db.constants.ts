819e75eb0c5f2033c07d976d2736e9a25690a795
0-BSCode <bryanipsanchez@gmail.com>
2024-11-11 08:51:52 +0800

    refactor(db constants): rename db constants to provide more context

--- packages/types/src/constants/db.constants.ts
+++ packages/types/src/constants/db.constants.ts
@@ -1,3 +1,7 @@
+// Transaction constants
 export const MAX_TRANSACTION_DESCRIPTION_LENGTH = 255
+
+// User constants
 export const MAX_USERNAME_LENGTH = 30
-export const MAX_VARCHAR_LENGTH = 255
+export const MAX_PASSWORD_LENGTH = 40
+export const MAX_PROFILE_PIC_LENGTH = 255
