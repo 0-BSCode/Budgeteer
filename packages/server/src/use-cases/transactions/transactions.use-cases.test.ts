d16d811e53b409cef6e3c1756e0d66c839a4932e
0-BSCode <bryanipsanchez@gmail.com>
2024-11-13 21:23:30 +0800

    test(vitest setup): use vite-tsconfig-paths for tsconfig aliasing
    If baseUrl is set in tsconfig, vite-tsconfig-paths must be used to allow vitest to work:
    https://vitest.dev/guide/common-errors.html#cannot-find-module-relative-path
    
    re TR-28

--- packages/server/src/use-cases/transactions/transactions.use-cases.test.ts
+++ packages/server/src/use-cases/transactions/transactions.use-cases.test.ts
@@ -1,5 +1,28 @@
-import { expect, test } from "vitest"
+import { expect, test, vi, describe } from "vitest"
+import { DataService } from "~/services/data-service"
+import { TransactionUseCases } from "./transactions.use-cases"
 
-test("1 + 2 equals 3", () => {
-  expect(1 + 2).toBe(3)
+vi.mock("~/services/data-service", () => ({
+  DataService: {
+    transactions: {
+      findById: vi.fn(),
+    },
+  },
+}))
+
+describe("TransactionUseCases", () => {
+  test("findById", async () => {
+    const transaction = {
+      id: 1,
+      description: "Test",
+      type: "Expense",
+      amount: 50,
+      category: "Groceries",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+    vi.mocked(DataService.transactions.findById).mockResolvedValue(transaction)
+    const response = await TransactionUseCases.findById(1)
+    expect(response.data).toEqual(transaction)
+  })
 })
