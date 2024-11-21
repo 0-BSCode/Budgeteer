064bc598a99391c2bc252c414e403246087035fe
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-11-21 13:01:55 +0800

    feat(tests): add error testing for auth
    re TR-26

--- packages/server/src/use-cases/auth/auth.use-cases.test.ts
+++ packages/server/src/use-cases/auth/auth.use-cases.test.ts
@@ -25,6 +25,11 @@ const GOOD_INPUT = {
   password: "12345678",
 }
 
+const BAD_INPUT = {
+  username: `test`,
+  password: "12345",
+}
+
 describe("register", () => {
   it("should create new accounts successfully & respond with an auth token", async () => {
     const mockRegisterResponse = { status: HttpStatusEnum.CREATED, token: "mocked.jwt.token" }
@@ -37,14 +42,46 @@ describe("register", () => {
 
     expect(token).toMatch(/^[\w-]+\.[\w-]+\.[\w-]+$/)
   })
+
+  it("should throw an error if the user already exists", async () => {
+    vi.mocked(AuthUseCases.register).mockRejectedValue(new Error("User already exists!"))
+
+    await expect(
+      AuthUseCases.register({ username: GOOD_INPUT.username, password: GOOD_INPUT.password }),
+    ).rejects.toThrowError()
+  })
+
+  it("should throw an error if the inputted pasword is too short", async () => {
+    vi.mocked(AuthUseCases.register).mockRejectedValue(new Error("Password too short!"))
+
+    await expect(
+      AuthUseCases.register({ username: GOOD_INPUT.username, password: BAD_INPUT.password }),
+    ).rejects.toThrowError()
+  })
 })
 
 describe("login", () => {
   it("should authenticate successfully and respond with an auth token", async () => {
-    const mockRegisterResponse = { status: HttpStatusEnum.OK, token: "mocked.jwt.token" }
-
+    const mockLoginResponse = { status: HttpStatusEnum.OK, token: "mocked.jwt.token" }
+    // const transaction: TransactionDto = {
+    //   id: 1,
+    //   description: "Test",
+    //   type: TransactionTypeEnum.EXPENSE,
+    //   amount: 50,
+    //   category: ExpenseCategoryEnum.ENTERTAINMENT,
+    //   createdAt: new Date(),
+    //   updatedAt: new Date(),
+    // }
+    // vi.mocked(DataService.transactions.create).mockResolvedValue(transaction)
+    // const response = await TransactionUseCases.create({
+    //   description: "Test",
+    //   type: TransactionTypeEnum.EXPENSE,
+    //   amount: 50,
+    //   category: ExpenseCategoryEnum.ENTERTAINMENT,
+    // })
+    // expect(response.data).toEqual(transaction)
     // Check if token has the structure of a JWT
-    vi.mocked(AuthUseCases.login).mockResolvedValue(mockRegisterResponse)
+    vi.mocked(AuthUseCases.login).mockResolvedValue(mockLoginResponse)
 
     // Attempt to log in with the same credentials used to register earlier
     const response = await AuthUseCases.login({ username: GOOD_INPUT.username, password: GOOD_INPUT.password })
@@ -52,5 +89,13 @@ describe("login", () => {
     const { token } = response
     // Check if token has the structure of a JWT
     expect(token).toMatch(/^[\w-]+\.[\w-]+\.[\w-]+$/)
+  })
+
+  it("should throw an error if login fails", async () => {
+    vi.mocked(AuthUseCases.login).mockRejectedValue(new Error("Login failed!"))
+
+    await expect(
+      AuthUseCases.login({ username: BAD_INPUT.username, password: BAD_INPUT.password }),
+    ).rejects.toThrowError()
   })
 })
