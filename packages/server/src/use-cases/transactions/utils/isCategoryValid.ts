370ec6c4b562382f2385a0af11cc6f8021427cbb
0-BSCode <bryanipsanchez@gmail.com>
2024-11-22 11:48:46 +0800

    refactor(transaction use case): refactor tests and utility functions to reflect new type changes
    re TR-6

--- packages/server/src/use-cases/transactions/utils/isCategoryValid.ts
+++ packages/server/src/use-cases/transactions/utils/isCategoryValid.ts
@@ -1,11 +1,9 @@
-import {
-  ExpenseCategoryEnum,
-  IncomeCategoryEnum,
-  TransactionTypeEnum,
-  type TransactionCategoryEnum,
-} from "@budgeteer/types"
+import { TransactionTypeEnum, IncomeCategoryEnum, ExpenseCategoryEnum } from "@budgeteer/types"
 
-export const isCategoryValid = (type: TransactionTypeEnum, category: TransactionCategoryEnum): boolean => {
+export const isCategoryValid = (
+  type: keyof typeof TransactionTypeEnum,
+  category: keyof typeof IncomeCategoryEnum | keyof typeof ExpenseCategoryEnum,
+): boolean => {
   const isValidExpenseCategory = type === TransactionTypeEnum.EXPENSE && category in ExpenseCategoryEnum
   const isValidIncomeCategory = type === TransactionTypeEnum.INCOME && category in IncomeCategoryEnum
 
