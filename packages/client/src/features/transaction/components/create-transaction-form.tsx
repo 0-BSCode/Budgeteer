17e9001fa0a94abf71d0bb730b84be2f663cc664
0-BSCode <bryanipsanchez@gmail.com>
2024-11-30 10:26:33 +0800

    feat(create transaction form): style the form
    re TR-17

--- packages/client/src/features/transaction/components/create-transaction-form.tsx
+++ packages/client/src/features/transaction/components/create-transaction-form.tsx
@@ -19,14 +19,12 @@ import { useToast } from "~/hooks/use-toast"
 import { convertToTitleCase } from "~/lib/convertToTitleCase"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form"
 import { Calendar } from "~/components/ui/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover"
 import { CalendarIcon } from "lucide-react"
 import { cn } from "~/lib/utils"
 import { format } from "date-fns"
-
-const DEFAULT_ID = -1
 
 export default function CreateTransactionForm() {
   const router = useRouter()
@@ -37,10 +35,6 @@ export default function CreateTransactionForm() {
 
   const form = useForm<TransactionCreateDto>({
     resolver: zodResolver(TransactionCreateDtoSchema),
-    defaultValues: {
-      // TODO: Don't include this (populated on the db)
-      userId: DEFAULT_ID,
-    },
   })
 
   const handleCancelCreating = () => {
@@ -75,9 +69,9 @@ export default function CreateTransactionForm() {
   }
 
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 flex flex-col items-center">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-y-4 w-full max-w-md">
           <FormField
             control={form.control}
             name="description"
@@ -108,14 +102,14 @@ export default function CreateTransactionForm() {
             control={form.control}
             name="date"
             render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Date of birth</FormLabel>
+              <FormItem className="flex flex-col gap-y-2">
+                <FormLabel>Transaction Date</FormLabel>
                 <Popover>
                   <PopoverTrigger asChild>
                     <FormControl>
                       <Button
                         variant={"outline"}
-                        className={cn("w-[240px] pl-3 text-left font-normal", !field.value && "text-muted-foreground")}
+                        className={cn("w-full pl-3 text-left font-normal", !field.value && "text-muted-foreground")}
                       >
                         {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
                         <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
@@ -132,7 +126,6 @@ export default function CreateTransactionForm() {
                     />
                   </PopoverContent>
                 </Popover>
-                <FormDescription>When did this transaction occur?</FormDescription>
                 <FormMessage />
               </FormItem>
             )}
@@ -150,7 +143,7 @@ export default function CreateTransactionForm() {
                   }}
                 >
                   <FormControl>
-                    <SelectTrigger>
+                    <SelectTrigger className="block w-full max-w-md rounded border border-input px-3 py-1">
                       <SelectValue placeholder="Select a type" />
                     </SelectTrigger>
                   </FormControl>
@@ -172,7 +165,7 @@ export default function CreateTransactionForm() {
                 <FormLabel>Category</FormLabel>
                 <Select onValueChange={field.onChange}>
                   <FormControl>
-                    <SelectTrigger>
+                    <SelectTrigger className="block w-full max-w-md rounded border border-input px-3 py-1">
                       <SelectValue placeholder="Select a category" />
                     </SelectTrigger>
                   </FormControl>
@@ -195,100 +188,18 @@ export default function CreateTransactionForm() {
               </FormItem>
             )}
           />
-          <Button type="submit" disabled={isLoading}>
-            Add Transaction
-          </Button>
+          <div className="space-y-4 w-full max-w-md py-8">
+            <div className="grid gap-3 w-full max-w-md">
+              <Button type="submit" disabled={isLoading}>
+                Add Transaction
+              </Button>
+              <Button variant="ghost" onClick={handleCancelCreating} className="w-full">
+                Cancel
+              </Button>
+            </div>
+          </div>
         </form>
       </Form>
-
-      {/* <div className="flex flex-col items-center gap-4">
-        <div className="w-full space-y-2 max-w-md">
-          <Label htmlFor="transaction-description">Description</Label>
-          <Input
-            id="transaction-description"
-            type="text"
-            value={description}
-            onChange={e => handleInputChange<string>(setDescription, e.target.value)}
-          />
-        </div>
-        <div className="w-full space-y-2 max-w-md">
-          <Label htmlFor="transaction-date">Date</Label>
-          <Input
-            id="transaction-date"
-            type="date"
-            className="max-w-md flex flex-col"
-            value={formatDate(date)}
-            onChange={e => handleInputChange<Date>(setDate, new Date(e.target.value))}
-          />
-        </div>
-        <div className="w-full space-y-2 max-w-md">
-          <Label htmlFor="transaction-amount">Amount</Label>
-          <Input
-            id="transaction-amount"
-            type="number"
-            className="max-w-md"
-            value={amount}
-            onChange={e => handleInputChange<string>(setAmount, e.target.value)}
-          />
-        </div>
-        <div className="w-full space-y-2 max-w-md">
-          <Label htmlFor="transaction-type" className="block">
-            Type
-          </Label>
-          <Select
-            value={type}
-            onValueChange={e =>
-              handleInputChange<TransactionTypeEnum>(setType, e as keyof typeof TransactionTypeEnumValues)
-            }
-          >
-            <SelectTrigger className="w-full max-w-md rounded border border-input px-3 py-1">
-              <SelectValue placeholder="Select a type" />
-            </SelectTrigger>
-            <SelectContent>
-              {Object.values(TransactionTypeEnumValues).map(type => (
-                <SelectItem key={type} value={type}>
-                  {convertToTitleCase(type)}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="w-full space-y-2 max-w-md">
-          <Label htmlFor="transaction-category" className="block">
-            Category
-          </Label>
-          <Select
-            value={category}
-            onValueChange={e => handleInputChange<TransactionCategoryEnum>(setCategory, e as TransactionCategoryEnum)}
-          >
-            <SelectTrigger className="w-full max-w-md rounded border border-input px-3 py-1">
-              <SelectValue placeholder="Select a category" />
-            </SelectTrigger>
-            <SelectContent>
-              {type === TransactionTypeEnumValues.EXPENSE &&
-                Object.values(ExpenseCategoryEnumValues).map(category => (
-                  <SelectItem key={category} value={category}>
-                    {convertToTitleCase(category)}
-                  </SelectItem>
-                ))}
-              {type === TransactionTypeEnumValues.INCOME &&
-                Object.values(IncomeCategoryEnumValues).map(category => (
-                  <SelectItem key={category} value={category}>
-                    {convertToTitleCase(category)}
-                  </SelectItem>
-                ))}
-            </SelectContent>
-          </Select>
-        </div>
-      </div> */}
-
-      <div className="flex justify-center">
-        <div className="grid gap-3 w-full max-w-md">
-          <Button variant="ghost" onClick={handleCancelCreating}>
-            Cancel
-          </Button>
-        </div>
-      </div>
     </div>
   )
 }
