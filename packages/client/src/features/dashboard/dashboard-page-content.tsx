e7a7d8243a4212a2d6d2a1482f67be1cde243acb
Luzefiru <ian.deejay.iv@gmail.com>
2024-11-30 13:22:36 +0800

    feat(dashboard): add generic stat widget

--- packages/client/src/features/dashboard/dashboard-page-content.tsx
+++ packages/client/src/features/dashboard/dashboard-page-content.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabsList, TabsTrigger } from "~/components/ui/tabs"
 import { TimeRangeEnumSchema, TimeRangeEnum } from "~/types/enums/TimeRangeEnum"
 import { convertToTitleCase } from "~/lib/convertToTitleCase"
 import { useRouter } from "next/navigation"
+import StatWidget from "./stat-widget"
 
 interface Props {
   initialTimeRange: TimeRangeEnum
@@ -22,8 +23,9 @@ export default function DashboardPageContent({ initialTimeRange }: Props) {
 
   return (
     <>
-      <div className="flex justify-between w-full items-center mb-8 flex-col sm:flex-row gap-4">
-        <h1 className="text-3xl font-bold text-start">Dashboard</h1>
+      <div className="flex justify-between w-full items-center mb-8 flex-col sm:flex-row gap-8">
+        <h1 className="hidden sm:block text-3xl font-bold text-start">Dashboard</h1>
+        <StatWidget className="sm:hidden" title="Net income" value="$56,381" description="31.74% up since last week" />
         <Tabs className="sm:w-1/2 w-full" onValueChange={handleTimeRangeChange} defaultValue={initialTimeRange}>
           <TabsList className="grid grid-cols-3">
             <TabsTrigger value={TimeRangeEnumSchema.Values.daily}>
