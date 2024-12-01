c77b624b13c477a962c6aacc68fb444c9c615b97
Luzefiru <ian.deejay.iv@gmail.com>
2024-12-01 18:20:25 +0800

    refactor: remove unnecessary indirection

--- packages/client/src/app/(app)/(uses-transactions)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { TransactionContextProvider } from "~/features/transaction/providers/transaction-provider"
-
-export default function UsesTransactionsLayout({ children }: { children: React.ReactNode }) {
-  return <TransactionContextProvider>{children}</TransactionContextProvider>
-}
