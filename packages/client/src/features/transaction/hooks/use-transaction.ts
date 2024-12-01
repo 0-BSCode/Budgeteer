6270714ad2794e9652240a85e79c00fc04b5a727
0-BSCode <bryanipsanchez@gmail.com>
2024-12-02 07:50:51 +0800

    feat(use-transaction): set up CRUD methods for transaction hook
    re TR-18

--- packages/client/src/features/transaction/hooks/use-transaction.ts
+++ packages/client/src/features/transaction/hooks/use-transaction.ts
@@ -1,4 +1,4 @@
-import { TransactionDto } from "@budgeteer/types"
+import { TransactionDto, TransactionUpdateDto } from "@budgeteer/types"
 import transactionService from "../services/transaction-service"
 import { RawTransactionCreateDto } from "~/types/entities/raw-transaction-create.dto"
 import useAuth from "~/features/auth/hooks/use-auth"
@@ -16,9 +16,18 @@ export default function useTransaction() {
     return transaction
   }
 
+  const getTransaction = async (id: string): Promise<TransactionDto> => {
+    if (!authToken) {
+      throw new Error("You cannot fetch transactions while authenticated! Please log in first.")
+    }
+
+    const transaction = await transactionService.getById(authToken, id)
+    return transaction
+  }
+
   const getAllTransactions = useCallback(async (): Promise<TransactionDto[]> => {
     if (!authToken) {
-      throw new Error("You cannot create a transaction while authenticated! Please log in first.")
+      throw new Error("You cannot fetch transactions while authenticated! Please log in first.")
     }
 
     const transactions = await transactionService.getAll(authToken)
@@ -26,8 +35,28 @@ export default function useTransaction() {
     return transactions
   }, [authToken])
 
+  const update = async (id: string, dto: TransactionUpdateDto): Promise<TransactionDto> => {
+    if (!authToken) {
+      throw new Error("You cannot update a transaction while authenticated! Please log in first.")
+    }
+
+    const transaction = await transactionService.update(authToken, id, dto)
+    return transaction
+  }
+
+  const remove = async (id: string): Promise<void> => {
+    if (!authToken) {
+      throw new Error("You cannot delete a transaction while authenticated! Please log in first.")
+    }
+
+    await transactionService.delete(authToken, id)
+  }
+
   return {
     create,
     getAllTransactions,
+    getTransaction,
+    update,
+    remove,
   }
 }
