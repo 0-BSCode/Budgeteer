733c00fd21733ed3e8e7a2664e67b954c18ff2d0
0-BSCode <bryanipsanchez@gmail.com>
2024-12-04 17:29:33 +0800

    feat(transaction-service): implement testing and proper error handling
    re TR-17, TR-18

--- packages/client/src/features/transaction/services/transaction-service.ts
+++ packages/client/src/features/transaction/services/transaction-service.ts
@@ -1,6 +1,12 @@
 import { z } from "zod"
-import { ResponseDto, TransactionDto, TransactionDtoSchema, TransactionUpdateDto } from "@budgeteer/types"
-import axios from "axios"
+import {
+  HttpStatusEnum,
+  ResponseDto,
+  TransactionDto,
+  TransactionDtoSchema,
+  TransactionUpdateDto,
+} from "@budgeteer/types"
+import axios, { AxiosError } from "axios"
 import config from "~/lib/config"
 import { RawTransactionCreateDto } from "~/types/entities/raw-transaction-create.dto"
 
@@ -8,13 +14,30 @@ const BASE_URL = `${config.NEXT_PUBLIC_API_BASE_URL}/transactions`
 
 const transactionService = {
   getAll: async (token: string): Promise<TransactionDto[]> => {
-    const { data: response } = await axios.get<ResponseDto<TransactionDto>>(BASE_URL, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    let data: TransactionDto[] | null
+    try {
+      const { data: response } = await axios.get<ResponseDto<TransactionDto[] | null>>(BASE_URL, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
 
-    const { data: transactions, success } = z.array(TransactionDtoSchema).safeParse(response.data)
+      if (response.status !== HttpStatusEnum.OK) {
+        throw new Error(response.message)
+      }
+
+      data = response.data
+    } catch (e) {
+      if ((e as Error).name === "AxiosError") {
+        if ((e as AxiosError).status) {
+          throw new Error((e as AxiosError).message)
+        }
+      }
+
+      throw new Error("Unable to fetch transactions.")
+    }
+
+    const { data: transactions, success } = z.array(TransactionDtoSchema).safeParse(data)
 
     if (!success) {
       throw new Error("The server sent malformatted data. Please contact the website admin.")
@@ -23,50 +46,109 @@ const transactionService = {
     return transactions
   },
   getById: async (token: string, id: string): Promise<TransactionDto> => {
-    const { data: response } = await axios.get<ResponseDto<TransactionDto>>(`${BASE_URL}/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    let data: TransactionDto | null
+    try {
+      const { data: response } = await axios.get<ResponseDto<TransactionDto | null>>(`${BASE_URL}/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
 
-    const transaction = validateResponseData(response.data)
+      if (response.status !== HttpStatusEnum.OK) {
+        throw new Error(response.message)
+      }
 
-    return transaction
+      data = response.data
+    } catch (e) {
+      if ((e as Error).name === "AxiosError") {
+        if ((e as AxiosError).status) {
+          throw new Error((e as AxiosError).message)
+        }
+      }
+
+      throw new Error("Unable to fetch transaction.")
+    }
+
+    return validateResponseData(data)
   },
   create: async (token: string, dto: RawTransactionCreateDto): Promise<TransactionDto> => {
-    const { data: response } = await axios.post<ResponseDto<TransactionDto>>(`${BASE_URL}`, dto, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
+    let data: TransactionDto | null
+    try {
+      const { data: response, status } = await axios.post<ResponseDto<TransactionDto | null>>(`${BASE_URL}`, dto, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
 
-    const transaction = validateResponseData(response.data)
+      if (response.status !== HttpStatusEnum.CREATED || status !== HttpStatusEnum.OK) {
+        throw new Error(response.message)
+      }
 
-    return transaction
+      data = response.data
+    } catch (e) {
+      if ((e as Error).name === "AxiosError") {
+        if ((e as AxiosError).status) {
+          throw new Error("AxiosError:" + (e as AxiosError).message)
+        }
+      }
+
+      throw new Error("Unable to create transaction: " + (e as Error).message)
+    }
+
+    return validateResponseData(data)
   },
   update: async (token: string, id: string, dto: TransactionUpdateDto): Promise<TransactionDto> => {
-    const { data: response } = await axios.patch<ResponseDto<TransactionDto>>(`${BASE_URL}/${id}`, dto, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
+    let data: TransactionDto | null
+    try {
+      const { data: response } = await axios.patch<ResponseDto<TransactionDto | null>>(`${BASE_URL}/${id}`, dto, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
 
-    const transaction = validateResponseData(response.data)
+      if (response.status !== HttpStatusEnum.OK) {
+        throw new Error(response.message)
+      }
 
-    return transaction
+      data = response.data
+    } catch (e) {
+      if ((e as Error).name === "AxiosError") {
+        if ((e as AxiosError).status) {
+          throw new Error("AxiosError: " + (e as AxiosError).message)
+        }
+      }
+
+      throw new Error("Unable to update transaction.")
+    }
+
+    return validateResponseData(data)
   },
   delete: async (token: string, id: string): Promise<void> => {
-    await axios.delete(`${BASE_URL}/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    try {
+      const { data: response } = await axios.delete<ResponseDto<null>>(`${BASE_URL}/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      if (response.status !== HttpStatusEnum.OK) {
+        throw new Error(response.message)
+      }
+    } catch (e) {
+      if ((e as Error).name === "AxiosError") {
+        if ((e as AxiosError).status) {
+          throw new Error((e as AxiosError).message)
+        }
+      }
+
+      throw new Error("Unable to delete transaction.")
+    }
   },
 }
 
-const validateResponseData = (data: TransactionDto): TransactionDto => {
+const validateResponseData = (data: TransactionDto | null): TransactionDto => {
   const { data: transaction, success } = TransactionDtoSchema.safeParse(data)
 
   if (!success) {
