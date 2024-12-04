12f83d8addc3ee4cbb1c0656f7747b1f54a9d9c7
0-BSCode <bryanipsanchez@gmail.com>
2024-12-04 17:27:22 +0800

    refactor(setup-test-user): set up test transaction to be used for transaction tests
    re TR-17, TR-18

--- packages/client/src/lib/test/setup-test-user.ts
+++ packages/client/src/lib/test/setup-test-user.ts
@@ -1,23 +1,54 @@
+import { ExpenseCategoryEnumValues, TransactionDto, TransactionTypeEnumValues } from "@budgeteer/types"
 import authService from "~/features/auth/services/auth-service"
+import transactionService from "~/features/transaction/services/transaction-service"
 
 export function createRandomString(): string {
   return Math.random().toString().slice(2, 10)
 }
 
-export const sharedTestState = {
+export const sharedUserTestState = {
   token: "",
   username: "",
   password: "",
 }
 
+export const sharedTransactionTestState: TransactionDto = {
+  id: 0,
+  amount: 0,
+  description: "",
+  date: new Date(),
+  category: ExpenseCategoryEnumValues.OTHER,
+  type: TransactionTypeEnumValues.EXPENSE,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  userId: 0,
+}
+
 export async function setupTestUser() {
-  if (!sharedTestState.token) {
+  if (!sharedUserTestState.token) {
     const username = `test${Math.random().toString().slice(2, 10)}`
     const password = "12345678"
     const token = await authService.register(username, password)
 
-    sharedTestState.token = token
-    sharedTestState.username = username
-    sharedTestState.password = password
+    sharedUserTestState.token = token
+    sharedUserTestState.username = username
+    sharedUserTestState.password = password
+
+    const transaction = await transactionService.create(sharedUserTestState.token, {
+      amount: sharedTransactionTestState.amount,
+      description: sharedTransactionTestState.description,
+      date: sharedTransactionTestState.date,
+      category: sharedTransactionTestState.category,
+      type: sharedTransactionTestState.type,
+    })
+
+    sharedTransactionTestState.id = transaction.id
+    sharedTransactionTestState.userId = transaction.userId
+    sharedTransactionTestState.createdAt = transaction.createdAt
+    sharedTransactionTestState.updatedAt = transaction.updatedAt
+
+    return
   }
 }
+
+// TODO: Add function to delete generated test data from db
