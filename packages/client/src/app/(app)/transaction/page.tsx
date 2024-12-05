7e5be8247292c964a9f85197f64add4d1caee127
0-BSCode <bryanipsanchez@gmail.com>
2024-12-05 13:28:27 +0800

    feat(transaction-query-page): set up files for transaction querying
    re TR-33

---
+++ packages/client/src/app/(app)/transaction/page.tsx
@@ -0,0 +1,8 @@
+import QueryTransactionTable from "~/features/transaction/components/query-transaction-table"
+
+export default function QueryTransactionPage() {
+  ;<div className="col-span-full">
+    <h1 className="mb-8 text-center text-3xl font-bold">Query Transactions</h1>
+    <QueryTransactionTable />
+  </div>
+}
---
+++ packages/client/src/features/transaction/components/query-transaction-table.tsx
@@ -0,0 +1,7 @@
+export default function QueryTransactionTable() {
+  return (
+    <div className="col-span-full">
+      <h1 className="mb-8 text-center text-3xl font-bold">Query Transactions</h1>
+    </div>
+  )
+}
