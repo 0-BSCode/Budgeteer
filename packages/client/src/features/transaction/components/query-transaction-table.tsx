41b2860e7d1df05be1134df540c2269b32465747
0-BSCode <bryanipsanchez@gmail.com>
2024-12-06 15:30:11 +0800

    feat(transaction-component): remove unnecessary component for transaction query table
    re TR-33

--- packages/client/src/features/transaction/components/query-transaction-table.tsx
+++
@@ -1,7 +0,0 @@
-export default function QueryTransactionTable() {
-  return (
-    <div className="col-span-full">
-      <h1 className="mb-8 text-center text-3xl font-bold">Query Transactions</h1>
-    </div>
-  )
-}
