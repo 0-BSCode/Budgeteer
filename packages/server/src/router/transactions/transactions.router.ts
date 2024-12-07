f25af801046ccdff924130a46a2788ce76c06ccd
0-BSCode <bryanipsanchez@gmail.com>
2024-12-07 15:04:15 +0800

    refactor(transactions-router): remove zod validators to throw custom HTTPExceptions
    Zod validators will return an early response on a parse failure. To customize the error to be shown
    on a failed parse, we removed zValidator and replaced it with custom validator logic

--- packages/server/src/router/transactions/transactions.router.ts
+++ packages/server/src/router/transactions/transactions.router.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono"
-import { zValidator } from "@hono/zod-validator"
 import { createTransactionSchema, transactionIdSchema, updateTransactionSchema } from "./transactions-dto.router"
 import { type TransactionCreateDto, type TransactionUpdateDto } from "@budgeteer/types"
 import { TransactionUseCases } from "~/use-cases/transactions/transactions.use-cases"
 import { authenticate } from "~/middleware/authenticate"
+import { validator } from "hono/validator"
+import { validateInput } from "../_utils/validate-input"
 
 type Variables = {
   id: string
@@ -13,8 +14,6 @@ const transactions = new Hono<{ Variables: Variables }>()
 
 transactions.use("*", authenticate)
 
-// TODO: Remove zValidator so that input validation errors
-// are handled by the global error handler as well
 transactions.get("/", async c => {
   const userId = c.get("id")
 
@@ -22,33 +21,41 @@ transactions.get("/", async c => {
   return c.json(response)
 })
 
-transactions.get("/:id", zValidator("param", transactionIdSchema), async c => {
-  const userId = parseInt(c.get("id"))
-  const id = c.req.param("id")
+transactions.get(
+  "/:id",
+  validator("param", (value, _c) => validateInput(transactionIdSchema, value)),
+  async c => {
+    const userId = parseInt(c.get("id"))
+    const id = c.req.param("id")
 
-  const response = await TransactionUseCases.findById(parseInt(id), userId)
-  return c.json(response)
-})
+    const response = await TransactionUseCases.findById(parseInt(id), userId)
+    return c.json(response)
+  },
+)
 
-transactions.post("/", zValidator("json", createTransactionSchema), async c => {
-  const { description, type, amount, category, date } = c.req.valid("json")
-  const data: TransactionCreateDto = {
-    description,
-    type,
-    amount,
-    category,
-    date,
-    userId: parseInt(c.get("id")),
-  }
+transactions.post(
+  "/",
+  validator("json", (value, _c) => validateInput(createTransactionSchema, value)),
+  async c => {
+    const { description, type, amount, category, date } = c.req.valid("json")
+    const data: TransactionCreateDto = {
+      description,
+      type,
+      amount,
+      category,
+      date,
+      userId: parseInt(c.get("id")),
+    }
 
-  const response = await TransactionUseCases.create(data)
-  return c.json(response)
-})
+    const response = await TransactionUseCases.create(data)
+    return c.json(response)
+  },
+)
 
 transactions.patch(
   "/:id",
-  zValidator("param", transactionIdSchema),
-  zValidator("json", updateTransactionSchema),
+  validator("param", (value, _c) => validateInput(transactionIdSchema, value)),
+  validator("json", (value, _c) => validateInput(updateTransactionSchema, value)),
   async c => {
     const { id } = c.req.valid("param")
     const userId = parseInt(c.get("id"))
@@ -67,12 +74,16 @@ transactions.patch(
   },
 )
 
-transactions.delete("/:id", zValidator("param", transactionIdSchema), async c => {
-  const { id } = c.req.valid("param")
-  const userId = parseInt(c.get("id"))
+transactions.delete(
+  "/:id",
+  validator("param", (value, _c) => validateInput(transactionIdSchema, value)),
+  async c => {
+    const { id } = c.req.valid("param")
+    const userId = parseInt(c.get("id"))
 
-  const response = await TransactionUseCases.delete(id, userId)
-  return c.json(response)
-})
+    const response = await TransactionUseCases.delete(id, userId)
+    return c.json(response)
+  },
+)
 
 export default transactions
